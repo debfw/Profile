66611785008338b492889a2053b5e149988ae4b8
69is54 <chienliko66@gmail.com>
2024-03-09 22:50:57 +0800

    feat: components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useTranslation } from "react-i18next";
 import "./App.css";
+import PrimarySearchAppBar from "./components/bar";
+import ActionAreaCard from "./components/card";
 
 function App() {
-  const { t } = useTranslation();
-
   return (
     <div className="max-w-4xl mx-auto p-5">
-      <h2 className="text-2xl font-bold mb-4">{t("profileTitle")}</h2>
-      <p className="mb-4">{t("profileDescription")}</p>
+      <PrimarySearchAppBar/>
+      <ActionAreaCard />
     </div>
   );
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,31 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+export default function ActionAreaCard() {
+  const { t } = useTranslation();
+  return (
+    <Card sx={{ maxWidth: 645 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="540"
+          image="155625133.png"
+          alt={t("name")}
+        />
+
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {t("title")}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {t("summary.focus")}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
