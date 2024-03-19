80a6d8a67601a5a396e9f2906a729a5eeb12c28e
69is54 <chienliko66@gmail.com>
2024-03-19 18:31:02 +0800

    fix: homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,33 @@
 // Import necessary libraries and components
-import {
-  Avatar,
-  Box,
-  Button,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Avatar, Box, Button, useMediaQuery, useTheme } from "@mui/material";
 import PrimarySearchAppBar from "../src/components/PrimarySearchAppBar";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps } from "next";
-import ProfileCard from "../src/components/ProfileCard";
 import { useCardContext } from "../src/contexts/CardContext";
 import ExperienceCard from "../src/components/Experience";
 import PortFolioCard from "../src/components/Portfolio";
 import SkillSetCard from "../src/components/Skillsets";
-import Header from "../src/components/header";
 import ChatSnackbar from "../src/components/ChatSnackbar";
+import StickyHeader from "../src/components/stickyHeader";
+import { useEffect, useState } from "react";
 
 function HomePage() {
+  const [flipAnimation, setFlipAnimation] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const bottom =
+        Math.ceil(window.innerHeight + window.scrollY) >=
+        document.documentElement.scrollHeight;
+      console.log(bottom);
+      if (bottom) {
+        setFlipAnimation(true);
+        setTimeout(() => setFlipAnimation(false), 1500);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   const {
     isExperienceVisible,
     isPortfolioVisible,
@@ -28,103 +37,104 @@ function HomePage() {
   } = useCardContext();
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const seeTools = (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        gap: "20px",
+        marginTop: "20px",
+      }}
+    >
+      <Box sx={{ flex: 1, maxWidth: "300px" }}>
+        {isExperienceVisible && <ExperienceCard />}
+      </Box>
+      <Box sx={{ flex: 1, maxWidth: "300px" }}>
+        {isPortfolioVisible && <PortFolioCard />}
+      </Box>
+      <Box sx={{ flex: 1, maxWidth: "300px" }}>
+        {isSkillSetVisible && <SkillSetCard />}
+      </Box>
+    </Box>
+  );
 
   return (
-    <Box sx={{ marginTop: "100px" }}>
+    <Box sx={{ marginTop: "100px", width: "-webkit-fill-available" }}>
       {isSmallScreen ? (
-        <Box sx={{ margin: 1 }}>
-          {" "}
-          <Header />
-          <PrimarySearchAppBar />
-          <ProfileCard />
-        </Box>
+        <>
+          <Box sx={{ margin: 1 }}>
+            <StickyHeader />
+          </Box>
+          <Box
+            sx={{
+              marginRight: 1,
+              position: "fixed",
+              zIndex: 100,
+              bottom: 20,
+              right: 2,
+              background: "linear-gradient(155deg, black, transparent)",
+            }}
+          >
+            <Button
+              onClick={toggleSnackbarVisibility}
+              sx={{ display: "flex", flexDirection: "column" }}
+            >
+              {isSnackbarVisible ? (
+                <Box sx={{ color: "white", fontSize: 40, padding: 2 }}>X</Box>
+              ) : (
+                <Box sx={{ color: "white", fontSize: 40, padding: 2 }}>
+                  CHAT
+                </Box>
+              )}
+            </Button>
+            {isSnackbarVisible && <ChatSnackbar />}
+          </Box>
+          <div
+            className={`page-flip-container ${
+              flipAnimation ? "animate-flip" : ""
+            }`}
+          >
+            {<h1>Hi</h1>}
+          </div>
+        </>
       ) : (
         <>
-          <Header />
-          <PrimarySearchAppBar />
-          <ProfileCard />
-        </>
-      )}
-
-      {isSmallScreen ? (
-        <Box
-          sx={{
-            marginRight: 1,
-            position: "fixed",
-            zIndex: 100,
-            display: "flex",
-            flexDirection: "row",
-            bottom: 20,
-            right: 2,
-          }}
-        >
-          <Button onClick={toggleSnackbarVisibility}>
-            {isSnackbarVisible ? null : (
+          <StickyHeader />
+          <Box
+            sx={{
+              marginRight: 1,
+              position: "fixed",
+              zIndex: 100,
+              bottom: 100,
+              right: 2,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Button
+              onClick={toggleSnackbarVisibility}
+              sx={{ display: "flex", flexDirection: "column" }}
+            >
               <Avatar
                 alt="Liko"
                 src="/155625133.png"
-                sx={{ width: "73px", height: "73px" }}
+                sx={{ width: "20%", height: "20%" }}
               />
-            )}
-          </Button>
-          {isSnackbarVisible && <ChatSnackbar />}
-        </Box>
-      ) : (
-        <Box
-          sx={{
-            marginRight: 1,
-            position: "fixed",
-            zIndex: 100,
-            bottom: 100,
-            right: 2,
-          }}
-        >
-          <Button onClick={toggleSnackbarVisibility}>
-            <Avatar
-              alt="Liko"
-              src="/155625133.png"
-              sx={{ width: "40%", height: "40%" }}
-            />{" "}
-            <Typography
-              sx={{
-                position: "absolute",
-                bottom: "60px",
-                right: "110px",
-                width: "50%",
-                color: "white",
-                bgcolor: "rgba(0, 0, 0, 0.5)",
-                textAlign: "center",
-                p: "4px", // This is the shorthand for padding in all directions
-              }}
-            >
-              {isSnackbarVisible
-                ? "Click again to close "
-                : "Click and ask me anything"}
-            </Typography>{" "}
-          </Button>
-          {isSnackbarVisible && <ChatSnackbar />}
-        </Box>
+              {isSnackbarVisible ? (
+                <Box sx={{ color: "black", fontSize: 40, padding: 2 }}>
+                  CLOSE
+                </Box>
+              ) : (
+                <Box sx={{ color: "black", fontSize: 40, padding: 2 }}>
+                  CHAT
+                </Box>
+              )}
+            </Button>
+            {isSnackbarVisible && <ChatSnackbar />}
+          </Box>
+        </>
       )}
-
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          gap: "20px",
-          marginTop: "20px",
-        }}
-      >
-        <Box sx={{ flex: 1, maxWidth: "300px" }}>
-          {isExperienceVisible && <ExperienceCard />}
-        </Box>
-        <Box sx={{ flex: 1, maxWidth: "300px" }}>
-          {isPortfolioVisible && <PortFolioCard />}
-        </Box>
-        <Box sx={{ flex: 1, maxWidth: "300px" }}>
-          {isSkillSetVisible && <SkillSetCard />}
-        </Box>
-      </Box>
     </Box>
   );
 }
---
+++ src/components/stickyHeader.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { useTranslation } from "react-i18next";
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function StickyHeader() {
+  const { t } = useTranslation();
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <CssBaseline />
+        <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+          <Typography variant="h2" component="h1" gutterBottom>
+            {t("summary.focus")}
+          </Typography>
+          <Typography variant="h5" component="h2" gutterBottom>
+            {t("summary.experience")}
+          </Typography>
+          <Typography variant="body1"> {t("email")}</Typography>
+          <Typography variant="body1"> {t("linkedin")}</Typography>
+        </Container>
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: "auto",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1">{t("name")} <Typography variant="h5">{t("title")}</Typography></Typography>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
